e0ccb16c84322684ae84bbd0c366c2c9a9870cd7
Malouni <swarealy@gmail.com>
2023-02-23 02:13:21 -0800

    Added css and front end for past reports

---
+++ pages/PastServeyReport.php
@@ -0,0 +1,69 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- links to stylesheets -->
+    <link rel="stylesheet" href="/fonts/arial-nova/stylesheet.css">
+    <link rel="stylesheet" href="/styles/generalStyles.css">
+    <link rel="stylesheet" href="../styles/PastServeyReport.css">
+
+    
+
+
+</head>
+
+<body>
+
+    <!-- Navigation section -->
+    <nav>
+        <img src="images/LOGOS/TRU-LG02.png" alt="logo">
+        <ul>
+            <li><a href="http://"><img src="images/icons/notifications.png" alt="notifications" class="icon"></a></li>
+            <li><a href="http://"><img src="images/icons/message.png" alt="messages" class="icon"></a></li>
+            <li><a href="http://"><span>UserName</span> <span>UserLastname</span></a></li>
+            <li><a href="http://"><img src="images/icons/menu.png" alt="menu" class="icon"></a>
+                <ul>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">Survey</a></li>
+                    <li><a href="">Log Out</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="pastReports">
+        <h3>Science Strategic Plan 2023</h3>
+        <div class="pastDocuments">
+            <div class="document">
+                <table>
+                    <tr>
+                        <td><p>Documents</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>Friday, 3 February 2022</p></td>
+                    </tr>
+                    <tr>
+                        <td><a href="" style="margin-right: 30xp;"> Strategic Plan Report (2022)</a> <img src="" class="download"> <img src="" class="download"></td>
+                    </tr>
+                    <tr>
+                        <td><p>Facility of Science</p></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
