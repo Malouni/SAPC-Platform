09ce55a378e471fc4882068d04889936d6413532
Malouni <swarealy@gmail.com>
2023-03-18 03:48:24 -0700

    Made some changes according to db

--- pages/PastSurveyReport.php
+++ pages/PastSurveyReport.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,35 +26,20 @@
 
 
     <div class="pastReports">
-
-        <!---------info section--------->
-        <p class="headers">Science Strategic Plan <span class="year">2023<span></p>
-        <div class="pastDocuments">
-            <div class="document">
-                <table>
-                    <tr class="rowDocuments">
-                        <td class="gridDocuments">
-                            <p class="document">Documents</p>
-                        </td>
-                    </tr>
-                    <tr class="rowDocuments">
-                        <td class="gridDocuments">
-                            <p class="date">Friday, 3 February 2022</p>
-                        </td>
-                    </tr>
-                    <tr class="rowDocuments">
-                        <td class="gridDocuments">
-                            <p class="SPRtext"> Strategic Plan Report (2022)</p>
-                        </td>
-                        <td><button class="pdf"><img src="../images/icons/pdf.png" alt="pdf" class="icon"></button></td>
-                    </tr>
-                    <tr class="rowDocuments">
-                        <td class="gridDocuments">
-                            <p class="facility">Facility of Science</p>
-                        </td>
-                    </tr>
-                </table>
-            </div>
+        <p class="headers">Science Strategic Plan 2023</p>
+        <div class="pastDocuments" id="documents-pane">
+            <?php
+                if (!empty($result))
+                    echo $result;
+                else
+                    echo "<div class='document'>
+                            <table>
+                                <tr class='rowDocuments'>
+                                    <td class='gridDocuments'><p class='document'>Error, no connection to the server</p></td>
+                                </tr>
+                            </table>
+                          </div> ";
+            ?>
         </div>
 
         <!--------------Goals tabs---------------->
@@ -66,112 +53,14 @@
                 <th class="gridGoals"><button class="buttonGoals" id="GSDbutton">Guided skill development</button></th>
             </tr>
         </table>
-        <div id="coverpattern"></div>
-
-        <!---------------------------DIEL report--------------------------------->
-        <div class="report" id="DIEL-R">
-            <p class="headers">Fostering diverse, inclusive and equitable learning and working environments</p>
-            <!--------------------SubGoal1------------------------>
-            <p class="headers">Inclusion and Diversity</p>
-            <p class="regularText">Ensure the staff and facility compliment is equitable, diverse and inclusive</p>
-            <table class="tablePastReport">
-                <tr class="rowPastReport">
-                    <th class="headerActivity">Activity</th>
-                    <th class="headerRest">Involvement</th>
-                    <th class="headerRest">Historical</th>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity">Indigenous presenters in classroom</td>
-                    <td class="gridRest">3</td>
-                    <td class="gridRest">+2</td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <!--add php code for adding a new row-->
-            </table>
-
-            <!--------------------SubGoal2------------------------>
-            <p class="headers">Community mindedness</p>
-            <p class="regularText">Promote, support and strengthen a respectful, inclusive, equitable and welcoming culture for all faculty, staff and students</p>
-
-            <table class="tablePastReport">
-                <tr class="rowPastReport">
-                    <th class="headerActivity">Activity</th>
-                    <th class="headerRest">Involvement</th>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-            </table>
-
-            <!--------------------SubGoal3------------------------>
-            <p class="headers">Curiosity</p>
-            <p class="regularText">Foster an environment that encourages interaction and shows respect to Indigenous learning and knowledge</p>
-
-            <table class="tablePastReport">
-                <tr class="rowPastReport">
-                    <th class="headerActivity">Activity</th>
-                    <th class="headerRest">Involvement</th>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-            </table>
-
-            <!--------------------SubGoal4------------------------>
-            <p class="headers">Sustainability</p>
-            <p class="regularText">Eliminate achievement gaps for marginalized students</p>
-
-            <table class="tablePastReport">
-                <tr class="rowPastReport">
-                    <th class="headerActivity">Activity</th>
-                    <th class="headerRest">Involvement</th>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-                <tr class="rowPastReport">
-                    <td class="gridActivity"> &nbsp; </td>
-                    <td class="gridRest"> &nbsp; </td>
-                </tr>
-            </table>
+        <div class="report" id="report-pane">
+            <?php
+                if (!empty($result))
+                    echo $result;
+                else
+                    echo "<p class='headers'>Error, no connection to the server</p>";
+            ?>
         </div>
-
-       
-
-
-
 
     </div>
 
