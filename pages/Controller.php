232e221cc2c6f42a47caa5704c9794394b07c30c
Malouni <swarealy@gmail.com>
2023-03-20 12:06:39 -0700

    Moved the navigation page to separate file

--- pages/Controller.php
+++ pages/Controller.php
@@ -15,8 +15,39 @@ require('model2.php');
 
 session_start();
 
+if($_POST['page'] == 'Navigation')
+{
+    if (!isset($_SESSION['LogIn'])) {
+        $display_type = 'none';
+        include('LogInPage.php');
+        exit();
+    }
 
-if ($_POST['page'] == 'LogInPage')
+    $command = $_POST['command'];
+    switch($command) {
+
+    case 'MainPage':
+        include('Index.php');
+        break;
+
+    case 'SurveyStart':
+        include('surveyStart.php');
+        break;
+
+    case 'PresentReport':
+        include('PastSurveyReport.php');
+        break;
+
+    case 'SignOut':
+        session_unset();
+        session_destroy();
+        $display_type = 'none';
+        include ('LogInPage.php');
+        break;
+    }
+}
+
+else if ($_POST['page'] == 'LogInPage')
 {
     $command = $_POST['command'];
     switch($command) {
@@ -56,12 +87,6 @@ else if ($_POST['page'] == 'MainPage')
 
             break;
 
-        case 'SignOut':
-            session_unset();
-            session_destroy();
-            $display_type = 'none';
-            include ('LogInPage.php');
-            break;
     }
 }
 else if ($_POST['page'] == 'PastReport')
@@ -78,12 +103,6 @@ else if ($_POST['page'] == 'PastReport')
 
             break;
 
-        case 'SignOut':
-            session_unset();
-            session_destroy();
-            $display_type = 'none';
-            include ('LogInPage.php');
-            break;
     }
 }
 else {
