875403f41458991c89a71d1d50f782f0d62cd7d7
alexalas2707 <alexalas2707@gmail.com>
2023-04-02 22:36:18 -0700

    Changes
    Changed survey and survey.css for Duy, created userReview and review model for user to see their answers, changed navigation javscript (to be corrected), changed generalstyless css so logout is at bottom of submenu, changed test data so we have an example we can use with everything

---
+++ pages/userReview.php
@@ -0,0 +1,44 @@
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey Review</title>
+
+    <!-- links to stylesheets -->
+    <link rel="stylesheet" href="/fonts/arial-nova/stylesheet.css">
+    <link rel="stylesheet" href="/styles/generalStyles.css">
+    <link rel="stylesheet" href="/styles/review.css">
+
+</head>
+
+<body>
+
+    <!-------------------------- Navigation section------------------------- -->
+    <?php
+    require('Navigation.php');
+    ?>
+    <!----------------------------- Main section -------------------------------->
+
+    <div class="container">
+
+        <div class="report">
+            <h1 class="sectionheader">User Answers</h1>
+            
+            <?php
+            require('../models/reviewmodel.php');
+            ?>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
