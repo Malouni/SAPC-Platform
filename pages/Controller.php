4d5d901d68bdff76150ea5d700dd9869b43ad76d
Malouni <swarealy@gmail.com>
2023-04-10 18:14:28 -0700

    Finished with uploading csv

--- pages/Controller.php
+++ pages/Controller.php
@@ -15,6 +15,7 @@ require('../models/MainPageModel.php');
 require('../models/SurveyModel.php');
 require('../models/surveyStartModel.php');
 require('../models/reviewmodel.php');
+require('../models/AdminPageModel.php');
 
 
 
@@ -76,10 +77,9 @@ else if($_POST['page'] == 'Navigation')
             include('userReview.php');
             break;
         }
-        
 
     case 'UploadCsv':
-        include('upload_csv.php');
+        include('AdminPage.php');
         break;
 
     case 'SignOut':
@@ -262,6 +262,146 @@ else if ($_POST['page'] == 'userReview')
             break;
     }
 }
+
+else if ($_POST['page'] == 'AdminPage')
+{
+    if (!isset($_SESSION['LogIn'])) {
+        $display_type = 'none';
+        include('LogInPage.php');
+        exit();
+    }
+
+    $command = $_POST['command'];
+    switch($command) {
+        case 'UserTable':
+            $result = get_users_info();
+            echo json_encode($result);
+            break;
+
+        case 'SurveyTable':
+            $result = get_surveys_info();
+            echo json_encode($result);
+            break;
+
+        case 'ChangeUser':
+            if($fieldToChange = get_the_column_name_user($_POST["choice"]))
+            {
+                if($result = update_user($_POST["rowId"], $fieldToChange, $_POST["value"]))
+                    $result = "The user's ".$_POST["choice"]." was updated successfully";
+                else
+                    $result = "Error occupied while updating the user, try again";
+            }
+            else
+                $result ="No such attribute for user";
+            echo json_encode($result);
+            break;
+
+        case 'ResetPassword':
+            if($result = rest_user_password($_POST['ResetPwdUserId']))
+                $result = "The password was set to DEFAULT for user: ";
+            else
+                $result = "Error occupied while updating the user, try again";
+            echo json_encode($result);
+            break;
+
+        case 'ChangeSurv':
+            if($fieldToChange = get_the_column_name_survey($_POST["choice"]))
+            {
+                if($result = update_survey($_POST["rowId"], $fieldToChange, $_POST["value"]))
+                    $result = "The survey field ".$_POST["choice"]." was updated successfully";
+                else
+                    $result = "Error occupied while updating the survey, try again";
+            }
+            else
+                $result ="No such attribute for survey";
+            echo json_encode($result);
+            break;
+
+        case 'AddNewUser':
+            if(!check_if_user_exists($_POST["Email"]))
+            {
+                if($_POST["Email"] == $_POST["EmailCon"])
+                {
+                    $result = add_new_user($_POST["Email"], $_POST["Fname"], $_POST["Lname"], $_POST["userType"], $_POST["department"]);
+                    if($result){
+                        $result = "New user ".$_POST["Email"]." was added successfully!";
+                    }else{
+                        $result = "The error occupied, try again later.";
+                    }
+                }
+                else
+                {
+                    $result = "Emails for user: ".$_POST["Email"]." are not matching, please try again";
+                }
+            }
+            else
+            {
+                $result = "User: ".$_POST["Email"]." already exists";
+            }
+            echo json_encode($result);
+            break;
+
+        case 'RemoveUser':
+            if($userName = remove_user($_POST["RemoveUserId"]))
+                $result = "The user: ".$userName." was deleted successfully!";
+            else
+                $result = "The error occupied, try again later.";
+            echo json_encode($result);
+            break;
+
+        case 'csvAddUsers':
+            $data = $_POST["csvFileDataUsers"];
+            $result = "";
+            for($line = 0; $line < count($data); $line++)
+            {
+                if(!check_if_user_exists($data[$line]["Username"]))
+                {
+                    if(add_new_user($data[$line]["Username"],$data[$line]["FirstName"], $data[$line]["LastName"], $data[$line]["UserType"], $data[$line]["Department"])){
+                        $result .= "The user ".$data[$line]["Username"]." was added successfully!\n";
+                    }else{
+                        $result .= "The user ".$data[$line]["Username"]." was not added due to the error!, try again\n";
+                    }
+                }
+                else
+                {
+                    $result .= "User already exists: ".$data[$line]["Username"]."\n";
+                }
+            }
+            echo json_encode($result);
+            break;
+
+        case 'csvAddSurvey':
+            $data = $_POST["csvFileDataSurveys"];
+            $result;
+            if(!check_if_survey_exists($data[0][0]["SurvYear"], $data[0][0]["SurvName"]))
+            {
+                $newSurvId = add_new_survey($data[0][0]["SurvYear"], $data[0][0]["SurvName"], $data[0][0]["SurvDateStart"], $data[0][0]["SurvDateEnd"], $data[0][0]["Position"]);
+                if($newSurvId)
+                {
+                    if($result = add_questions_to_new_survey($data[1], $data[2], $newSurvId))
+                    {
+                        $result = "The survey: ".$data[0][0]["SurvName"]." was added successfully!";
+                    }
+                    else
+                    {
+                        $result = "The survey: ".$data[0][0]["SurvName"]." addition failed, check the csv file (Questions Part)";
+                    }
+                }
+                else
+                {
+                    $result = "The survey: ".$data[0][0]["SurvName"]." addition failed, check the csv file (Survey Details)";
+                }
+            }
+            else
+            {
+                $result ="The survey: ".$data[0][0]["SurvName"]." already exists";
+            }
+            echo json_encode($result);
+            break;
+    }
+}
+
+
 else{
 
 }
---
+++ scripts/AdminPage.js
@@ -0,0 +1,388 @@
+document.addEventListener("DOMContentLoaded", function(){
+    showUsersTable();
+    showSurveysTable();
+});
+
+
+//Functions
+function showUsersTable(){
+    var url = 'Controller.php';
+
+    var query = {page: 'AdminPage', command: 'UserTable'};
+
+    $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+
+        var userTable = "";
+
+        userTable += "<table border='1'>";
+        userTable += "<thead>";
+        userTable += "<tr>";
+        userTable += "<th>User First Name</th>";
+        userTable += "<th>User Last Name</th>";
+        userTable += "<th>Institutional Email</th>";
+        userTable += "<th>User Type</th>";
+        userTable += "<th>Department</th>";
+        userTable += "<th>Reset Password</th>";
+        userTable += "<th>Delete</th>";
+        userTable += "</tr>";
+        userTable += "</thead>";
+        userTable += "<tbody>";
+
+        if(result == "NoResults")
+        {
+            userTable += "<tr>";
+            userTable += "<td>No</td>";
+            userTable += "<td>Users</td>";
+            userTable += "<td>Found</td>";
+            userTable += "<td></td>";
+            userTable += "<td></td>";
+            userTable += "<td></td>";
+            userTable += "<td></td>";
+            userTable += "</tr>";
+        }
+        else
+        {
+            for (var row = 0; row < result.length; row++) {
+
+                userTable += "<tr id='" + result[row]['UserID'] + "'>";
+                userTable += "<td name='First Name' onclick='changeTheClickedFieldUserTable(this.id)' id='Fname" + result[row]['UserID'] + "'>" + result[row]['Fname'] + "</td>";
+                userTable += "<td name='Last Name' onclick='changeTheClickedFieldUserTable(this.id)' id='Lname" + result[row]['UserID'] + "'>" + result[row]['Lname'] + "</td>";
+                userTable += "<td name='Email' onclick='changeTheClickedFieldUserTable(this.id)' id='UserName" + result[row]['UserID'] + "'>" + result[row]['UserName'] + "</td>";
+                userTable += "<td name='Position' onclick='changeTheClickedFieldUserTable(this.id)' id='Position" + result[row]['UserID'] + "'>" + result[row]['Position'] + "</td>";
+                userTable += "<td name='Department' onclick='changeTheClickedFieldUserTable(this.id)' id='Department" + result[row]['UserID'] + "'>" + result[row]['Department'] + "</td>";
+                userTable += "<td class='buttonForEdit'><button class='editDelete' button-Reset-UserId='" + result[row]['UserID'] + "'>Reset</button></td>";
+                userTable += "<td class='buttonForEdit'><button class='editDelete' button-Delete-UserId='" + result[row]['UserID'] + "'>Delete</button></td>";
+                userTable += "</tr>";
+            }
+        }
+        userTable += "</tbody></table>";
+
+        $('#user-table').html(userTable);
+
+        $('td > button[button-Reset-UserId]').click(function() {
+            var userId = $(this).attr('button-Reset-UserId');
+            var userName = $("#UserName"+userId+"").text();
+            let text = "Are you sure, you want to reset password to DEFAULT for user: "+userName+"?";
+            if (confirm(text) == true){
+                resetUserPassword(userId, userName);
+            }
+        });
+
+        $('td > button[button-Delete-UserId]').click(function() {
+            var userId = $(this).attr('button-Delete-UserId');
+            var userName = $("#UserName"+userId+"").text();
+            let text = "Are you sure, you want to delete user: "+userName+"?";
+            if (confirm(text) == true){
+                removeUserFunction(userId);
+            }
+            showUsersTable();
+        });
+    });
+}
+
+function changeTheClickedFieldUserTable(clicked_id)
+{
+    var rowId = $("#"+clicked_id+"").closest('tr').attr("id");
+    var choice = $("#"+clicked_id+"").attr('name');
+    let newData = prompt("Enter new "+ choice +"", $("#"+clicked_id+"").text());
+    if (newData)
+    {
+        var url = 'Controller.php';
+        var query = {page: 'AdminPage', command: 'ChangeUser', choice: choice, rowId: rowId, value: newData};
+        $.post(url, query, function(data) {
+            var result = JSON.parse(data);
+            alert(result);
+            showUsersTable();
+        });
+    }
+}
+
+function showSurveysTable(){
+    var url = 'Controller.php';
+
+    var query = {page: 'AdminPage', command: 'SurveyTable'};
+
+    $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+
+        var surveyTable = "";
+
+        surveyTable += "<table border='1'>";
+        surveyTable += "<thead>";
+        surveyTable += "<tr>";
+        surveyTable += "<th>Survey Name</th>";
+        surveyTable += "<th>Survey Year</th>";
+        surveyTable += "<th>Survey Start Date</th>";
+        surveyTable += "<th>Survey End Date</th>";
+        surveyTable += "<th>User Access</th>";
+        surveyTable += "<th>Delete</th>";
+        surveyTable += "</tr>";
+        surveyTable += "</thead>";
+        surveyTable += "<tbody>";
+
+        if(result == "NoResults")
+        {
+            surveyTable += "<tr>";
+            surveyTable += "<td>No</td>";
+            surveyTable += "<td>Surveys</td>";
+            surveyTable += "<td>Found</td>";
+            surveyTable += "<td></td>";
+            surveyTable += "<td></td>";
+            surveyTable += "<td></td>";
+            surveyTable += "</tr>";
+        }
+        else
+        {
+            for (var row = 0; row < result.length; row++) {
+                surveyTable += "<tr id='" + result[row]['SurvID'] + "'>";
+                surveyTable += "<td name='Survey Name' onclick='changeTheClickedFieldSurveyTable(this.id)' id='SurvName"+ result[row]['SurvID'] +"'>" + result[row]['SurvName'] + "</td>";
+                surveyTable += "<td name='Survey Year' onclick='changeTheClickedFieldSurveyTable(this.id)' id='SurvYear"+ result[row]['SurvID'] +"'>" + result[row]['SurvYear'] + "</td>";
+                surveyTable += "<td name='Survey Date Start' onclick='changeTheClickedFieldSurveyTable(this.id)' id='SurvDateStart"+ result[row]['SurvID'] +"'>" + result[row]['SurvDateStart'] + "</td>";
+                surveyTable += "<td name='Survey Date End' onclick='changeTheClickedFieldSurveyTable(this.id)' id='SurvDateEnd"+ result[row]['SurvID'] +"'>" + result[row]['SurvDateEnd'] + "</td>";
+                surveyTable += "<td name='Position' onclick='changeTheClickedFieldSurveyTable(this.id)' id='Position"+ result[row]['SurvID'] +"'>" + result[row]['Position'] + "</td>";
+                surveyTable += "<td class='buttonForEdit'><button class='editDelete' button-Delete-SurveyId='" + result[row]['SurvID'] + "'>Delete</button></td>";
+                surveyTable += "</tr>";
+            }
+        }
+        surveyTable += "</tbody></table>";
+
+        $('#survey-table').html(surveyTable);
+
+        $('td > button[button-Delete-SurveyId]').click(function() {
+            var suvId= $(this).attr('button-Delete-SurveyId');
+            var survName = $("#SurvName"+suvId+"").text();
+            let text = "Are you sure, you want to delete the Survey: "+survName+"?";
+            if (confirm(text) == true){
+                removeUserFunction(suvId);
+                showSurveysTable();
+            }
+        });
+    });
+}
+
+function changeTheClickedFieldSurveyTable(clicked_id)
+{
+    var rowId = $("#"+clicked_id+"").closest('tr').attr("id");
+    var choice = $("#"+clicked_id+"").attr('name');
+    let newData = prompt("Enter new "+ choice +"", $("#"+clicked_id+"").text());
+    if (newData)
+    {
+        var url = 'Controller.php';
+        var query = {page: 'AdminPage', command: 'ChangeSurv', choice: choice, rowId: rowId, value: newData};
+        $.post(url, query, function(data) {
+            var result = JSON.parse(data);
+            alert(result);
+            showSurveysTable();
+        });
+    }
+}
+
+function addNewUserFunction()
+{
+    var url = 'Controller.php';
+    var query = {page: 'AdminPage', command: 'AddNewUser', Fname: $("#fName").val(),
+                                                           Lname: $("#lName").val(),
+                                                           Email: $("#email").val(),
+                                                           EmailCon: $("#emailC").val(),
+                                                           department: $("#department").val(),
+                                                           userType: $("#userType").val()};
+    $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+        alert(result);
+        showUsersTable();
+    });
+}
+
+function removeUserFunction(userId)
+{
+    var url = 'Controller.php';
+    var query = {page: 'AdminPage', command: 'RemoveUser', RemoveUserId: userId};
+    $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+        alert(result);
+        showUsersTable();
+    });
+}
+
+function resetUserPassword(userId,userName)
+{
+    var url = 'Controller.php';
+    var query = {page: 'AdminPage', command: 'ResetPassword', ResetPwdUserId: userId};
+    $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+        alert(result+userName);
+        showUsersTable();
+    });
+}
+
+function addUsersFromFileFunction(){
+    var csvFile = document.querySelector('#csvUserFile').files[0];
+    var reader = new FileReader();
+    reader.readAsText(csvFile);
+    reader.onload = function (e) {
+        var text = e.target.result;
+        var data = csvToArrayUsers(text);
+        //var result = csvToArrayUsers(text);
+        //var data = JSON.stringify(result);
+
+        var url = 'Controller.php';
+        var query = {page: 'AdminPage', command: 'csvAddUsers', csvFileDataUsers: data};
+        $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+        alert(result);
+        showUsersTable();
+        });
+    };
+}
+
+function csvToArrayUsers(text)
+{
+    var rows = text.split(/\r?\n|\r/);
+    rows = rows.filter(item => item);
+    var array = [];
+    var headers;
+    var delimiter = ",";
+    var cells;
+
+    for(singleRow = 0; singleRow < rows.length; singleRow++)
+    {
+        if(singleRow == 0)
+        {
+            headers = rows[singleRow].split(delimiter);
+            //Filters headers array from empty elements
+            headers = headers.filter(item => item);
+            singleRow++;
+        }
+        cells = rows[singleRow].split(delimiter);
+
+        var values = rows[singleRow].split(delimiter);
+        values = values.filter(item => item);
+        array.push(
+            headers.reduce(function (object, header, index) {
+                object[header] = values[index];
+                return object;
+                }, {})
+        );
+
+    }
+    return array;
+}
+
+function addSurveyFromFileFunction(){
+    var csvFile = document.querySelector('#csvSurveyFile').files[0];
+    var reader = new FileReader();
+    reader.readAsText(csvFile);
+
+    reader.onload = function (e) {
+        var text = e.target.result;
+        var data = csvToArraySurvey(text);
+        //var result = JSON.stringify(data);
+        //alert(result);
+        var url = 'Controller.php';
+        var query = {page: 'AdminPage', command: 'csvAddSurvey', csvFileDataSurveys: data};
+        $.post(url, query, function(data) {
+        var result = JSON.parse(data);
+        alert(result);
+        showSurveysTable();
+        });
+    };
+}
+
+function csvToArraySurvey(text)
+{
+    var dataSectionAmount = 3;
+    var rows = text.split(/\r?\n|\r/);
+    rows = rows.filter(item => item);
+    var array = [dataSectionAmount];
+    var surveyInfoArray = [];
+    var questionArray = [];
+    var subQuestionArray = [];
+    var headers;
+    var arrayOfPossibleAns = [];
+    var possibleAnsPositionHolder = false;
+    var dataSection;
+    var delimiter = ",";
+    var cells;
+
+    for(singleRow = 0; singleRow < rows.length; singleRow++)
+    {
+        cells = rows[singleRow].split(delimiter);
+        for(singleCell = 0; singleCell < cells.length; singleCell++)
+        {
+            //alert(rows[singleRow]);
+            if(singleCell == 0 && (cells[singleCell] == "SurvYear" || cells[singleCell] == "Goal" || cells[singleCell] == "Question"))
+            {
+                headers = rows[singleRow].split(delimiter);
+                //Filters headers array from empty elements
+                headers = headers.filter(item => item);
+                if(singleCell == 0 && cells[singleCell] == "SurvYear")
+                {
+                    dataSection = 0;
+                }
+                else if(singleCell == 0 && cells[singleCell] == "Goal")
+                {
+                    dataSection = 1;
+                }
+                else if(singleCell == 0 && cells[singleCell] == "Question")
+                {
+                    dataSection = 2;
+                }
+                singleRow++;
+            }
+
+            if(cells[singleCell] == "PossibleAnswers")
+            {
+                possibleAnsPositionHolder = singleCell;
+            }
+            else if(possibleAnsPositionHolder && singleCell >= possibleAnsPositionHolder)
+            {
+                arrayOfPossibleAns[singleCell - possibleAnsPositionHolder] = cells[singleCell];
+            }
+        }
+        cells = rows[singleRow].split(delimiter);
+        for(possibleAns = 0; possibleAns < (rows.length - possibleAnsPositionHolder - 1); possibleAns++)
+        {
+            arrayOfPossibleAns[possibleAns] = cells[possibleAnsPositionHolder+possibleAns];
+        }
+        arrayOfPossibleAns = arrayOfPossibleAns.filter(item => item);
+        if(dataSection == 0)
+        {
+            var values = rows[singleRow].split(delimiter);
+            values = values.filter(item => item);
+            surveyInfoArray.push(
+                headers.reduce(function (object, header, index) {
+                    object[header] = values[index];
+                    return object;
+                    }, {})
+            );
+        }
+        else if(dataSection == 1)
+        {
+            var values = rows[singleRow].split(delimiter, possibleAnsPositionHolder + 1);
+            values[possibleAnsPositionHolder] = arrayOfPossibleAns;
+            questionArray.push(
+                headers.reduce(function (object, header, index) {
+                    object[header] = values[index];
+                    return object;
+                    }, {})
+            );
+        }
+        else if(dataSection == 2)
+        {
+            var values = rows[singleRow].split(delimiter, possibleAnsPositionHolder + 1);
+            values[possibleAnsPositionHolder] = arrayOfPossibleAns;
+            subQuestionArray.push(
+                headers.reduce(function (object, header, index) {
+                    object[header] = values[index];
+                    return object;
+                    }, {})
+            );
+        }
+        arrayOfPossibleAns = [];
+    }
+    array[0] = surveyInfoArray;
+    array[1] = questionArray;
+    array[2] = subQuestionArray;
+    return array;
+}
